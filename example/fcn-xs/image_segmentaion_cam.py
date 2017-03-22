8c3b040caa56137b62c975b0b11828cc9dca50fb
xilaili <xli47@ncsu.edu>
2017-03-22 02:58:26 -0400

    changed fcnxs demo

--- example/fcn-xs/image_segmentaion_cam.py
+++ example/fcn-xs/image_segmentaion_cam.py
@@ -1,4 +1,5 @@
 # pylint: skip-file
+import cv2
 import numpy as np
 from PIL import Image
 from timeit import default_timer as timer
@@ -29,11 +30,11 @@ model_previx = "model/FCN8s_VGG16"
 epoch = 19
 ctx = mx.gpu(0)
 
-def get_data(img_path):
+def get_data(img):
     """get the (1, 3, h, w) np.array data for the img_path"""
     mean = np.array([123.68, 116.779, 103.939])  # (R,G,B)
-    img = Image.open(img_path)
-    img = np.array(img, dtype=np.float32)
+    #img = Image.open(img_path)
+    #img = np.array(img, dtype=np.float32)
     reshaped_mean = mean.reshape(1, 1, 3)
     img = img - reshaped_mean
     img = np.swapaxes(img, 0, 2)
@@ -43,20 +44,29 @@ def get_data(img_path):
 
 def main():
     fcnxs, fcnxs_args, fcnxs_auxs = mx.model.load_checkpoint(model_previx, epoch)
-    start = timer()
-    fcnxs_args["data"] = mx.nd.array(get_data(img), ctx)
-    data_shape = fcnxs_args["data"].shape
-    label_shape = (1, data_shape[2]*data_shape[3])
-    fcnxs_args["softmax_label"] = mx.nd.empty(label_shape, ctx)
-    exector = fcnxs.bind(ctx, fcnxs_args ,args_grad=None, grad_req="null", aux_states=fcnxs_args)
-    exector.forward(is_train=False)
-    output = exector.outputs[0]
-    out_img = np.uint8(np.squeeze(output.asnumpy().argmax(axis=1)))
-    time_elapsed = timer() - start
-    out_img = Image.fromarray(out_img)
-    out_img.putpalette(pallete)
-    out_img.save(seg)
-    print "Detection time: {:.6f} sec".format(time_elapsed)
+    video = cv2.VideoCapture(0)
+
+    count = 0
+    while True:
+        _, img = video.read()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) 
+        start = timer()
+        fcnxs_args["data"] = mx.nd.array(get_data(img), ctx)
+        data_shape = fcnxs_args["data"].shape
+        label_shape = (1, data_shape[2]*data_shape[3])
+        fcnxs_args["softmax_label"] = mx.nd.empty(label_shape, ctx)
+        exector = fcnxs.bind(ctx, fcnxs_args ,args_grad=None, grad_req="null", aux_states=fcnxs_args)
+        exector.forward(is_train=False)
+        output = exector.outputs[0]
+        out_img = np.uint8(np.squeeze(output.asnumpy().argmax(axis=1)))
+        time_elapsed = timer() - start
+        out_img = Image.fromarray(out_img)
+        out_img.putpalette(pallete)
+        fname = 'data/'+str(count).zfill(7)+'.png'
+        out_img.save(fname)
+        count += 1
+        #out_img.save(seg)
+        print "Detection time: {:.6f} sec".format(time_elapsed)
 
 if __name__ == "__main__":
     main()
